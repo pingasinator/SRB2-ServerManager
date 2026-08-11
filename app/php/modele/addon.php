832dfc9fe82754a797ecb23cf2139d5c815d52eb
yanni <yanni.ajil@gmail.com>
2026-08-12 00:23:27 +0200

    E

--- app/php/modele/addon.php
+++ app/php/modele/addon.php
@@ -128,13 +128,15 @@ class AddonModel {
                     if($level['ID']  != null){
                         if(str_contains($row," = ")){
                             $data =  explode(" = ",$row);
-                            if(str_contains($data[1], ",")){
-                                $values = explode(",",$data[1]);
-                                foreach ($values as $value){
-                                    $level[$data[0]][] = ucfirst($value);
+                            if($level[$data[0]] != null){
+                                if(str_contains($data[1], ",")){
+                                    $values = explode(",",$data[1]);
+                                    foreach ($values as $value){
+                                        $level[$data[0]][] = ucfirst($value);
+                                    }
+                                }else{
+                                    $level[$data[0]] = ucfirst($data[1]);
                                 }
-                            }else{
-                                $level[$data[0]] = ucfirst($data[1]);
                             }
                         }
                     }
